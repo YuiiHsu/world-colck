61577359eb7d263704b9289eaf02b8af3cc2257a
YuiiHsu <54923318+YuiiHsu@users.noreply.github.com>
2022-03-09 18:58:33 +0800

    add uuid4v

--- src/App.js
+++ src/App.js
@@ -1,56 +1,68 @@
+import { useEffect, useState } from 'react';
 import style from './App.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  let clockList = {
+  const [clockList, setClockList] = useState({
     newYork: {
       city: "NEW YORK",
       timeZone: 'America/New_York',
-      locales: 'en-us',
-      date: '28 JAN. 2019',
-      time: '02:46'
+      date: '',
+      time: ''
     },
     london: {
       city: "LONDON",
-      timeZone: 'Britain/London',
-      locales: 'en-gb',
-      date: '28 JAN. 2019',
-      time: '02:46'
+      timeZone: 'Europe/London',
+      date: '',
+      time: ''
     },
     bangkok: {
       city: "BANGKOK",
-      timeZone: 'Thailand/Bangkok',
-      locales: 'th-TH',
-      date: '28 JAN. 2019',
-      time: '02:46'
+      timeZone: 'Asia/Bangkok',
+      date: '',
+      time: ''
     },
     taiwan: {
       city: "TAIWAN",
-      timeZone: 'America/New_York',
-      locales: 'zh-tw',
-      date: '28 JAN. 2019',
-      time: '02:46'
+      timeZone: 'Asia/Taipei',
+      date: '',
+      time: ''
     },
     sydney: {
       city: "SYDNEY",
       timeZone: 'Australia/Sydney',
-      locales: 'en-au',
-      date: '28 JAN. 2019',
-      time: '02:46'
+      date: '',
+      time: ''
     }
-  }
+  });
 
-  const getUTCDate = () => {
-    const clock = clockList.newYork
-    // 取得當地時間
-    // day/month/year, hour/min/sec AM/PM
-    const date = new Date().toLocaleString(clock.locales, { timeZone: clock.timeZone }).split(',');
-    const dateArray = date[0] ? date[0].split('/') : [];
-    const timeArray = date[1] ? date[1].substring(0, date[1].length - 3).split(':') : [];
-    // 轉成UTC時間
-    const utc = new Date(Date.UTC(dateArray[2], dateArray[0], dateArray[1]));
-    const newTime = Object.values(clockList).find(c => c.city === clock.city);
-    newTime.date = date.date;
-    newTime.time = date.time;
+  const getDate = (clockData) => {
+    const dataList = clockData;
+    const option = {
+      year: "numeric",
+      month: "short",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZone: ''
+    }
+
+    Object.values(dataList).forEach(data => {
+
+      option.timeZone = data.timeZone;
+
+      // 取得當地時間 e.g., Feb 23, 2022, 05:28 AMs
+      const currentDate = new Date().toLocaleString("en-us", option).split(', ');
+      const newData = data;
+      const dateArray = currentDate[0].split(' ');
+      const time = currentDate[2] ? currentDate[2].substring(0, 5) : '';
+
+      data.date = `${dateArray[1]} ${dateArray[0]} ${currentDate[1]}`;
+      data.time = `${time}`;
+      // Object.assign(newClockData, newData);
+    })
+
+    setClockList(dataList);
   }
 
   /**
@@ -63,18 +75,21 @@ function App() {
   }
 
 
+  useEffect(() => {
+    setInterval((() => getDate(clockList)), 1000);
+  }, []);
+
   return (
     <div className={style.App}>
       <div className={style.container}>
         <div className={style.title}>
           WORLD CLOCK
-          {getUTCDate()}
         </div>
         {
           Object.values(clockList)
             .map(clock =>
               <div
-                key={clock.city}
+                key={uuidv4()}
                 className={isBlackBeground(clock)
                   ? style.blackBackground
                   : style.whiteBackground} >
